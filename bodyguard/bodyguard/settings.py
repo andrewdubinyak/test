0c948c88b972ad97984d1a676e049cdf413efa39
Andriy <andriy@MacBook-Pro-Andriy.local>
2019-11-20 15:19:37 +0200

    comit

--- bodyguard/bodyguard/settings.py
+++ bodyguard/bodyguard/settings.py
@@ -84,11 +84,14 @@ WSGI_APPLICATION = 'bodyguard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bodyguard',
+        'USER': 'admin',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
